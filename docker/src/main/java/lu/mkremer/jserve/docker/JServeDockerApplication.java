894d04bff93c356ce3cb53c293dc79105238d28f
Michel Kremer <kremi151dev@gmail.com>
2021-09-22 22:38:26 +0200

    Add docker module

---
+++ docker/src/main/java/lu/mkremer/jserve/docker/JServeDockerApplication.java
@@ -0,0 +1,31 @@
+package lu.mkremer.jserve.docker;
+
+import lu.mkremer.jserve.JServe;
+import lu.mkremer.jserve.conf.ServerConfiguration;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JServeDockerApplication {
+
+	public static void main(String[] args) {
+		Path rootPath = Paths.get("/", "opt", "jserve");
+		Path filesPath = rootPath.resolve("files");
+
+		if (!Files.isDirectory(filesPath)) {
+			throw new IllegalStateException("No readable volume mounted at " + filesPath);
+		}
+
+		String portStr = System.getenv("JSERVE_PORT");
+
+		ServerConfiguration config = new ServerConfiguration();
+		config.setServePath(filesPath.toString());
+		if (portStr != null && !portStr.isEmpty()) {
+			config.setPort(Integer.parseInt(portStr));
+		}
+
+		new JServe(config).start();
+	}
+
+}
