2c6d75341d2dda81fa93982baa1cc8fd4571d52a
Michel Kremer <kremi151dev@gmail.com>
2021-09-28 16:25:21 +0200

    Use buildx to build Docker images

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "Build Docker image"
-docker build -t kremi151/jserve:latest .
+# For debugging purposes
+echo "Is release build? ${JSERVE_IS_RELEASE}"
+
+if [ -z "${JSERVE_BUILDX_PLATFORMS:-}" ]; then
+    echo "Build Docker image"
+    docker build -t kremi151/jserve:latest .
+elif
+    echo "Build Docker image using buildx"
+    docker buildx build --platform $JSERVE_BUILDX_PLATFORMS --tag kremi151/jserve:latest
+fi
