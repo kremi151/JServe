8b3e6604c3072923bc031a2591bbddf5efc492a7
Michel Kremer <kremi151dev@gmail.com>
2021-09-28 16:44:35 +0200

    Push using buildx

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# For debugging purposes
-set -x
-echo "Is release build? ${JSERVE_IS_RELEASE}"
-
 if [ -z "${JSERVE_BUILDX_PLATFORMS:-}" ]; then
     echo "Build Docker image"
     docker build -t kremi151/jserve:latest .
+
+    if [[ "${JSERVE_PUBLISH:-}" == "true" ]]; then
+        docker push kremi151/jserve:latest
+    fi
 else
     echo "Build Docker image using buildx"
-    docker buildx build --platform $JSERVE_BUILDX_PLATFORMS --tag kremi151/jserve:latest .
+
+    JSERVE_BUILDX_ARGS="--tag kremi151/jserve:latest"
+
+    if [[ "${JSERVE_PUBLISH:-}" == "true" ]]; then
+        JSERVE_BUILDX_ARGS="${JSERVE_BUILDX_ARGS} --push"
+    fi
+
+    docker buildx build --platform $JSERVE_BUILDX_PLATFORMS $JSERVE_BUILDX_ARGS .
 fi
