354b9be4d142da6b071e53d720080134c98b5017
Michel Kremer <kremi151dev@gmail.com>
2021-09-28 16:28:58 +0200

    Fix syntax error

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -6,7 +6,7 @@ echo "Is release build? ${JSERVE_IS_RELEASE}"
 if [ -z "${JSERVE_BUILDX_PLATFORMS:-}" ]; then
     echo "Build Docker image"
     docker build -t kremi151/jserve:latest .
-elif
+else
     echo "Build Docker image using buildx"
     docker buildx build --platform $JSERVE_BUILDX_PLATFORMS --tag kremi151/jserve:latest
 fi
