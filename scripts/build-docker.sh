1a60f7db5872261846e3f01bf3cff9eddc044173
Michel Kremer <kremi151dev@gmail.com>
2021-09-28 18:36:51 +0200

    Tag images with version number

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+JSERVE_VERSION=$(./gradlew printVersion -q)
+
+echo "JServe version is ${JSERVE_VERSION}"
+
 if [ -z "${JSERVE_BUILDX_PLATFORMS:-}" ]; then
     echo "Build Docker image"
-    docker build -t kremi151/jserve:latest .
+    docker build -t kremi151/jserve:${JSERVE_VERSION} .
+	docker tag kremi151/jserve:${JSERVE_VERSION} kremi151/jserve:latest
 
     if [[ "${JSERVE_PUBLISH:-}" == "true" ]]; then
+        docker push kremi151/jserve:${JSERVE_VERSION}
         docker push kremi151/jserve:latest
     fi
 else
     echo "Build Docker image using buildx"
 
-    JSERVE_BUILDX_ARGS="--memory 1g --tag kremi151/jserve:latest"
+    JSERVE_BUILDX_ARGS="--memory 1g --tag kremi151/jserve:latest --tag kremi151/jserve:${JSERVE_VERSION}"
 
     if [[ "${JSERVE_PUBLISH:-}" == "true" ]]; then
         JSERVE_BUILDX_ARGS="${JSERVE_BUILDX_ARGS} --push"
